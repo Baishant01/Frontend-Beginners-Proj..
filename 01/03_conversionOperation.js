07f19a4adad7dec2e2db25e0c0017daeab529cbb
Baishant01 <baishant005@gmail.com>
2025-02-09 11:51:43 +0000

    Conversion of data types

---
+++ 01/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "99abc";
+// // let number = null;
+// let number = '';
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+// console.log(number);
+
+// let valueOfNumber = Number(number);
+// console.log(valueOfNumber);
+
+// // CONVERSION TO NUMBER 
+// // '99' ==> 99
+// // '99abc' ==> NaN
+// // '' ==> 0
+// // true ==> 1
+// // false ==> 0
+
+
+// let loggedIn = 1; // FOR ALL EMPTY VALUE, CONVERSION INTO BOOLEAN RESULTS TO FALSE
+
+// let numberToBool = Boolean(loggedIn);
+// console.log(numberToBool);
+
+
+let aNumber = 100;
+let numberToString = String(aNumber);
+
+console.log(numberToString);
+console.log(typeof(numberToString));
