abfa24f1fc4454425f81ee5e7e9aaebd3cc12645
Baishant01 <baishant005@gmail.com>
2025-02-09 14:02:42 +0000

    strings

---
+++ 01/05_strings.js
@@ -0,0 +1,9 @@
+const name ="Baishant ";
+const repoCount = 5;
+
+// console.log(name + repoCount);
+console.log(`Hello, my name is ${name} and I have ${repoCount} repos in my Github.`);
+
+let chessUserName = new String('baishant_np');
+console.log(chessUserName);
+console.log(name.toUpperCase());
