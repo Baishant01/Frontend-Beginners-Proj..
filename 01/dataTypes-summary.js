37557ecf6c4d4d20912e172a90983402183ec1d3
Baishant01 <baishant005@gmail.com>
2025-02-09 13:37:30 +0000

    data type summary modified

--- 01/dataTypes-summary.js
+++ 01/dataTypes-summary.js
@@ -35,6 +35,51 @@
 
     }
 
-    console.table(([typeof heros, typeof myObj, typeof myFunction]));
+    // console.table(([typeof heros, typeof myObj, typeof myFunction]));
 
 
+    /* ******************************************************************************************************** */
+
+    //  Stack (Primitive), Heap (Non-Primitive)
+
+
+    let myName = 'Baishant';
+    let myFullName = myName;
+    myFullName = 'Baishant Pachhai'
+
+    // console.log(myName);
+    // console.log(myFullName);
+
+
+    let userOne = {
+        myName : 'Baisant Pachhai',
+        myAge : 21,
+        myEmail : 'Baishant005@gmail.com',
+    }
+
+    let userTwo = userOne;
+
+    userTwo.myEmail = 'pbaishant@gmail.com';
+
+    // console.log(userTwo);
+    // console.log(userOne);
+
+    let function1 = function myFunction () {
+
+        let myAge = 21;
+        console.log(myAge);
+    }
+
+    let function2 = function1;
+
+    function2.myAge = 22;
+    // console.log(function2);
+    // console.log(function1);
+
+    let arr1 = [1,2,3,4,5];
+    let arr2 = arr1;
+
+    arr2.unshift(0);
+
+    console.log(arr2);
+    console.log(arr1);
