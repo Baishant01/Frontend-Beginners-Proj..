8b5cf3e9356a5291b43b7ccf7ea3387007f79467
Baishant01 <baishant005@gmail.com>
2025-08-13 10:45:37 +0545

    Delete 01 directory

--- 01/02_dataTypes.js
+++
@@ -1,4 +0,0 @@
-"use strict"; // treat all JS code as newer version
-
-// alert('hello'); // WE ARE USING NODE JS NOT BROWSER
-
--- 01/05_strings.js
+++
@@ -1,9 +0,0 @@
-const name ="Baishant ";
-const repoCount = 5;
-
-// console.log(name + repoCount);
-console.log(`Hello, my name is ${name} and I have ${repoCount} repos in my Github.`);
-
-let chessUserName = new String('baishant_np');
-console.log(chessUserName);
-console.log(name.toUpperCase());
